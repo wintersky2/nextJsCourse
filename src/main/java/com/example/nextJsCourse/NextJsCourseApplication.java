3e80acd95cd80aff239c0bf25457012b2724367d
wintersky2 <emsw747@gmail.com>
2024-03-28 14:56:11 +0900

    git init

---
+++ src/main/java/com/example/nextJsCourse/NextJsCourseApplication.java
@@ -0,0 +1,13 @@
+package com.example.nextJsCourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextJsCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextJsCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nextJsCourse/NextJsCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nextJsCourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextJsCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
