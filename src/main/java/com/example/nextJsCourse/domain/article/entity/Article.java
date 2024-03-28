b5d06dacb789d8e1a06a11b601209051298ede7c
wintersky2 <emsw747@gmail.com>
2024-03-28 15:12:49 +0900

    articles get method

---
+++ src/main/java/com/example/nextJsCourse/domain/article/entity/Article.java
@@ -0,0 +1,10 @@
+package com.example.nextJsCourse.domain.article.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Article {
+    private Long id;
+}
